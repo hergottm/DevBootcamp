b18bd3012dcfb844996d414475bb31af6f33e783
Matt Hergott <mhergy@gmail.com>
2018-02-12 02:07:38 +0000

    created public directory and created a stylesheet, tell express to serve the public directory, made a directory called partials which includes the html header and footer of each page

--- Section_24/moreexpress/ejsdemo/app.js
+++ Section_24/moreexpress/ejsdemo/app.js
@@ -1,15 +1,32 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.render("index.ejs");
+    res.render("index");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res){
     var thing = req.params.thing;
-    res.render("love.ejs", {thingVar: thing});
+    res.render("love", {thingVar: thing});
 });
 
+
+app.get("/posts", function(req, res) {
+    var posts = [
+        {title: "Post 1", author: "Frank"},
+        {title: "My pet bunny", author: "Jack"},
+        {title: "Can you believe this Frenchy!", author: "Matt"}
+        ];
+    
+    res.render("posts", {posts: posts});
+    
+    
+});
+
+//listener
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is listening");
 });
