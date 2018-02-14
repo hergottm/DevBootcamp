40c3beebc2f1756726eb369fa245dda871b9432b
Matt Hergott <mhergy@gmail.com>
2018-02-14 16:48:50 +0000

    added a new route called show and it follows the RESTful routing scheme and displays information bout a single campsite. Also learned how to drop a table in mondoDB

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -11,14 +11,16 @@ app.set("view engine", "ejs");
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create({
-//     name: "Salmon Creek", 
-//     image:"https://images.unsplash.com/photo-1437382944886-45a9f73d4158?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3895efc1fd5d2fb67acdaee4b5d9c463&auto=format&fit=crop&w=1950&q=80"
+//     name: "Granite Hill", 
+//     image:"https://images.unsplash.com/photo-1437382944886-45a9f73d4158?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3895efc1fd5d2fb67acdaee4b5d9c463&auto=format&fit=crop&w=1950&q=80",
+//     description: "This is a huge granite hill, no bathrooms, no water beautiful granite!"
 // }, function(error, campground){
 //     if(error){
 //         console.log(error);
@@ -29,30 +31,31 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 // });
 
 
-// root route
+
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 
-// campground route
+// INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res){
     // Get all campground from DB
     Campground.find({}, function(error, allCampgrounds){
         if(error){
             console.log(error);
         } else {
-            res.render("campgrounds", {campgrounds:allCampgrounds} );
+            res.render("index", {campgrounds:allCampgrounds} );
         }
     })
 });
 
-// post route
+// CREATE - add new campground to DB
 app.post("/campgrounds", function(req,res){
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image}
+    var desc = req.body.description
+    var newCampground = {name: name, image: image, description: desc}
     // Create a new campground and save to DB
     Campground.create(newCampground, function(error, newlyCreated){
         if(error){
@@ -63,12 +66,36 @@ app.post("/campgrounds", function(req,res){
     });
 });
 
+
+// NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res) {
     res.render("new.ejs");
 })
+
+// SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res) {
+    // find campground with provided ID
+    Campground.findById(req.params.id, function(error, foundCampground){
+       if(error){
+           console.log(error);
+       } else {
+               // render show template with that campground
+           res.render("show", {campground: foundCampground});
+       }
+    });
+});
 
 
 // listener
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The Yelp Camp server has started!");
 });
+
+
+// // Restful Routes
+// name        url         verb        desc.
+// ========================================================
+// INDEX       /dogs       GET         Display a list of all dogs
+// NEW         /dogs/new   GET         Displays form to make a new dogs
+// CREATE      /dogs       POST        Add new dog to DB
+// SHOW        /dogs/:id   GET         Shows info about one dog
